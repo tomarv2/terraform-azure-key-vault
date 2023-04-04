b91f481a14f56f7f298bff9119d6e565a2607fcf
tomarv2 <varuntomar2019@gmail.com>
2023-04-04 04:54:35 +0000

    Sync from management repo

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "global" {
-  source = "git::git@github.com:tomarv2/terraform-global.git//common?ref=v0.0.1"
+  source = "github.com/tomarv2/terraform-global.git//common?ref=v0.0.1"
 }
 
 data "azurerm_client_config" "current" {}
